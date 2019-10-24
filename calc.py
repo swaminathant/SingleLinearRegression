524aea031169b17826537c483677e2dc262c983b
swaminathant <56918932+swaminathant@users.noreply.github.com>
2019-10-24 10:25:48 +0530

    Calc file for doing predection

---
+++ calc.py
@@ -0,0 +1,12 @@
+from ml import linearregression
+
+ln = linearregression.SinglelinearRegression()
+
+age = [18,25,57,45,26,64,37,40,24,33]
+salary = [15000,29000,68000,52000,32000,80000,41000,45000,26000,33000]
+
+ypredict = ln.Linear(age,salary)
+print(ypredict)
+print("Predict age 22 salary is :",ln.Predict(22))
+print("RMSE Value is :",ln.RMSE(ypredict, salary))
+print("Pearsonr value is : ",ln.pearsonr(age,salary))
