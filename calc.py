866d83ff7084d2b6769688694f1c122fe3059387
swaminathant <56918932+swaminathant@users.noreply.github.com>
2019-10-24 15:15:03 +0530

    Add files via upload
    Changes made added the beta0 and beta1 to print on console.

--- calc.py
+++ calc.py
@@ -6,6 +6,7 @@ age = [18,25,57,45,26,64,37,40,24,33]
 salary = [15000,29000,68000,52000,32000,80000,41000,45000,26000,33000]
 
 ypredict = ln.Linear(age,salary)
+print(ln.B1, ln.B0)
 print(ypredict)
 print("Predict age 22 salary is :",ln.Predict(22))
 print("RMSE Value is :",ln.RMSE(ypredict, salary))
